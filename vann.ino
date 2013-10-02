68785112d32221fc7a8392a5c2994115050670a8
Martin Stensgård <mastensg@ping.uio.no>
2013-10-02 21:37:44 +0200

    read and write
    Signed-off-by: Martin Stensgård <mastensg@ping.uio.no>

--- vann.ino
+++ vann.ino
@@ -1,4 +1,11 @@
+const int a = 11;
+const int b = 12;
 const int led = 13;
+const int in = A0;
+
+const int slowness = 8;
+
+int last = 0;
 
 void
 setup()
@@ -13,8 +20,29 @@ setup()
 void
 loop()
 {
-    while (!Serial.available())
-        ;
+    char c;
 
-    digitalWrite(led, HIGH);
+    while (!Serial.available())
+    {
+        if (millis() >> slowness == last)
+            continue;
+
+        last = millis() >> slowness;
+
+        Serial.println(analogRead(in));
+    }
+
+    c = Serial.read();
+
+    switch (c)
+    {
+    case 'a': digitalWrite(a,   LOW);   break;
+    case 'A': digitalWrite(a,   HIGH);  break;
+
+    case 'b': digitalWrite(b,   LOW);   break;
+    case 'B': digitalWrite(b,   HIGH);  break;
+
+    case 'l': digitalWrite(led, LOW);   break;
+    case 'L': digitalWrite(led, HIGH);  break;
+    }
 }
